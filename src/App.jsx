c577b8fd1c017276d065072a08de85597a5e179c
Abdullah Zakariyya <66228619+Abdillahzakkie@users.noreply.github.com>
2022-07-16 22:50:12 +0100

    added /me route

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
+import MyAccount from "./pages/MyAccount";
 import Trade from "./pages/Trade";
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
 			<Routes>
 				<Route exact path="/" element={<Home />} />
 				<Route path="/trade/token/:tokenAddress" element={<Trade />} />
+				<Route path="/me" element={<MyAccount />} />
 			</Routes>
 		</div>
 	);
