5c8b3be23faf76b855309c9e6be6052980de0c95
Abdullah Zakariyya <66228619+Abdillahzakkie@users.noreply.github.com>
2022-07-17 13:23:13 +0100

    added ScrollTop and Footer component

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
 import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import Navbar from "./components/Navbar";
+import ScrollTop from "./components/ScrollTop";
 import Home from "./pages/Home";
 import MyAccount from "./pages/MyAccount";
 import Trade from "./pages/Trade";
+import { handleScrollEffect } from "./components/helpers/handleScrollEffect";
+import Footer from "./components/Footer";
 
 function App() {
 	return (
@@ -14,8 +17,12 @@ function App() {
 				<Route path="/trade/token/:tokenAddress" element={<Trade />} />
 				<Route path="/me" element={<MyAccount />} />
 			</Routes>
+			<ScrollTop />
+			<Footer />
 		</div>
 	);
 }
+
+document.addEventListener("scroll", handleScrollEffect);
 
 export default App;
