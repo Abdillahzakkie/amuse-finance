80960b3be5e6c6a51fc085a80149c517e2d39c74
Abdullah Zakariyya <66228619+Abdillahzakkie@users.noreply.github.com>
2022-07-18 11:00:15 +0100

    home page component

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,8 +1,135 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import Typewriter from "typewriter-effect";
+
 import { HomeWrapper } from "./style";
 
+import img1 from "../../assets/others/Amuse icon 1.png";
+import img2 from "../../assets/others/Amuse icon 2.png";
+import img3 from "../../assets/others/Amuse icon 3.png";
+import img4 from "../../assets/others/Amuse icon 4.png";
+import dashboardPreview from "../../assets/others/Amuse SS.jpg";
+
 function Home() {
-	return <HomeWrapper className="grid"></HomeWrapper>;
+	const handleWhitePaperDownload = (e) => {
+		e.preventDefault();
+	};
+
+	return (
+		<HomeWrapper className="grid">
+			<section className="grid banner">
+				<section className="grid typewritter-effect">
+					<div>
+						<h1>100% Cashbacks On</h1>
+						<h1>
+							<Typewriter
+								options={{
+									strings: ["Ethereum Gas Spends", "Rewards for trading"],
+									autoStart: true,
+									loop: true,
+								}}
+							/>
+						</h1>
+						<p>From the Dev of TweetsZone.com</p>
+						<div className="grid btns">
+							<Link to="/presale">
+								<button type="button" className="active">
+									Presale
+								</button>
+							</Link>
+							{/* <a
+								href="#"
+								// download={"Amuse Finance white paper"}
+								onClick={handleWhitePaperDownload}
+							>
+							</a> */}
+							<button
+								type="button"
+								className="whitepaper"
+								onClick={handleWhitePaperDownload}
+							>
+								Whitepaper
+							</button>
+						</div>
+					</div>
+				</section>
+				<section className="grid custom">
+					<div className="grid circle" />
+					<div className="dashboard-preview">
+						<div className="grid image">
+							<img src={dashboardPreview} alt="dashboard previewer" />
+						</div>
+					</div>
+				</section>
+			</section>
+
+			<section className="grid features-container">
+				<header className="grid">
+					<h1 className="text-4xl md:text-5xl font-semibold pb-5">Features</h1>
+					<p>
+						Amuse is a sustainable cashback protocol, fully decentralised which
+						is made to augment spent gas on transactions for Ethereum users.
+						Hodlers Amuse token will also benefit from numerous incentives that
+						has been algorithmically written in the contract.
+					</p>
+				</header>
+				<div className="grid card-container">
+					<div className="grid wrapper">
+						<div className="grid card rounded shadow-lg m-3 transition transform duration-200 ease-in hover:-translate-y-3">
+							<div className="grid img">
+								<img src={img1} alt="description" />
+							</div>
+							<div className="grid header">
+								<h1>hodl</h1>
+							</div>
+							<div className="grid detail">
+								<p>Earn 1% AMD rewards every 24hours for holding</p>
+							</div>
+						</div>
+
+						<div className="grid card rounded shadow-lg m-3 transition transform duration-200 ease-in hover:-translate-y-3 text-blue-600">
+							<div className="grid img">
+								<img src={img2} alt="description" />
+							</div>
+							<div className="grid header">
+								<h1>trade</h1>
+							</div>
+							<div className="grid detail">
+								<p>Earn passive cashback for holding and trading AMD</p>
+							</div>
+						</div>
+
+						<div className="grid card rounded shadow-lg m-3 transition transform duration-200 ease-in hover:-translate-y-3">
+							<div className="grid img">
+								<img src={img3} alt="description" />
+							</div>
+							<div className="grid header">
+								<h1>stake</h1>
+							</div>
+							<div className="grid detail">
+								<p>Earn 1% in ETHER for all staked AMD per 24hours</p>
+							</div>
+						</div>
+
+						<div className="grid card rounded shadow-lg m-3 transition transform duration-200 ease-in hover:-translate-y-3">
+							<div className="grid img">
+								<img src={img4} alt="description" />
+							</div>
+							<div className="grid header">
+								<h1>referral</h1>
+							</div>
+							<div className="grid detail">
+								<p>
+									Earn as much as 25% of the transaction fees paid by referree
+									during buy actions
+								</p>
+							</div>
+						</div>
+					</div>
+				</div>
+			</section>
+		</HomeWrapper>
+	);
 }
 
 export default Home;
--- src/pages/Home/style.js
+++ src/pages/Home/style.js
@@ -4,10 +4,270 @@ export const HomeWrapper = styled.div`
 	margin-top: var(--nav-height);
 	width: 100%;
 	height: 100%;
+	position: relative;
 
-	header {
+	.banner {
+		height: 75vh;
+		width: 100%;
+		grid-template-columns: repeat(2, 1fr);
+		grid-gap: 2em 1em;
+
+		.typewritter-effect {
+			width: 100%;
+			margin: auto;
+			padding: 0 0 0 6em;
+
+			div {
+				line-height: 1.5;
+				margin: auto;
+				width: 100%;
+				color: var(--mainBlue);
+
+				h1 {
+					font-size: 2em;
+					font-weight: 900;
+					letter-spacing: var(--letter-spacing);
+				}
+
+				p {
+					font-size: 1.2em;
+				}
+
+				.btns {
+					margin: 2em 0;
+					button {
+						background: var(--mainBlue);
+						outline: none;
+						width: 40%;
+						margin: 0 0 1em;
+						border-radius: 5em;
+						color: var(--white);
+						text-align: center;
+						font-size: 1.05em;
+						padding: 1em;
+						border: 0.1em solid var(--mainBlue);
+						transition: var(--transition);
+						font-family: Inter, Georgia, "Times New Roman", Times, serif;
+
+						&:hover {
+							background: transparent;
+							color: var(--mainBlue);
+							border: 0.1em solid var(--mainBlue);
+						}
+					}
+
+					.active {
+						background: transparent;
+						color: var(--mainBlue);
+
+						&:hover {
+							background: var(--mainBlue);
+							color: var(--white);
+						}
+					}
+				}
+			}
+		}
+
+		.custom {
+			width: 100%;
+			height: 100%;
+			place-items: center;
+			position: relative;
+
+			.circle {
+				width: 60%;
+				height: 80%;
+				border-radius: 100%;
+				background: var(--mainBlue);
+				margin: auto 0;
+				margin-right: 0;
+			}
+
+			.dashboard-preview {
+				position: absolute;
+				top: 0;
+				right: 0;
+				height: 100%;
+
+				.image {
+					width: 100%;
+					height: 100%;
+					place-items: end;
+					align-items: center;
+
+					img {
+						width: 66%;
+						height: 66%;
+					}
+				}
+			}
+		}
+	}
+
+	.features-container {
+		background: var(--mainBlue);
 		place-items: center;
-		grid-row: 2;
-		letter-spacing: 1px;
+		padding: 2em 0;
+		width: 100%;
+
+		header {
+			letter-spacing: var(--letter-spacing);
+			line-height: 1.75;
+			place-items: center;
+			color: var(--white);
+			padding: 2em;
+			width: 60%;
+			text-align: center;
+
+			h1 {
+				font-size: 2em;
+				text-transform: capitalize;
+			}
+		}
+
+		.card-container {
+			width: 100%;
+			place-items: center;
+			padding: 0 1em 0 0;
+			user-select: none;
+
+			.wrapper {
+				grid-template-columns: repeat(auto-fit, minmax(270px, 1fr));
+				grid-gap: 2em;
+				width: 100%;
+				padding: 2em;
+				margin: 0 auto;
+
+				.card {
+					width: 100%;
+					background: var(--white);
+					color: var(--darkGrey);
+					border-radius: 1em;
+					padding: 2em;
+					grid-gap: 1em 0;
+
+					:hover {
+						background: var(--hoverBlue);
+					}
+
+					.img {
+						width: 100%;
+						height: 100%;
+
+						img {
+							width: 40px;
+							height: 40px;
+						}
+					}
+
+					.header {
+						width: 100%;
+						text-transform: capitalize;
+						font-size: 1.5em;
+						letter-spacing: var(--letter-spacing);
+						font-weight: 800;
+						place-items: start;
+					}
+
+					.detail {
+						width: 100%;
+						font-size: 1em;
+						font-weight: 500;
+						letter-spacing: var(--letter-spacing);
+					}
+				}
+			}
+		}
+	}
+
+	@media (max-width: 1439px) {
+		& {
+			.banner {
+				.typewritter-effect {
+					div {
+						.btns {
+							button {
+								width: 65%;
+							}
+						}
+					}
+				}
+			}
+
+			.chart-container {
+				grid-template-columns: 1fr;
+			}
+		}
+
+		@media (max-width: 1023px) {
+			& {
+				.banner {
+					.typewritter-effect {
+						padding: 0 0 0 2em;
+
+						div {
+							.btns {
+								a {
+									button {
+										width: 70%;
+										padding: 1.25em 0.5em;
+									}
+								}
+							}
+						}
+					}
+				}
+			}
+
+			@media (max-width: 767px) {
+				& {
+					.banner {
+						grid-template-columns: 1fr;
+
+						.typewritter-effect {
+							grid-template-columns: repeat(12, 1fr);
+
+							div {
+								grid-column: 1/13;
+
+								.btns {
+									width: 100%;
+									place-items: start;
+									margin: 2em auto;
+									padding: auto;
+
+									a {
+										width: 70%;
+										button {
+											width: 100%;
+										}
+									}
+								}
+							}
+						}
+
+						.custom {
+							display: none;
+						}
+					}
+
+					.features-container {
+						border-bottom: 0.1em solid var(--white);
+
+						header {
+							width: 100%;
+						}
+						.card-container {
+							padding: 0 0em 0 0;
+						}
+					}
+
+					.chart-container {
+						display: none;
+					}
+				}
+			}
+		}
 	}
 `;
